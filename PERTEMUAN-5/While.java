4e1e5b72d53236c5198052c874dea3c5ffbe15b3
caseyshalom <caseysinpc@gmail.com>
2026-04-28 09:20:40 +0700

    upload

---
+++ PERTEMUAN-5/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int bil = 10;
+        while (bil >= 1) {
+            System.out.println(bil);
+            --bil;
+        }
+    }
+}
